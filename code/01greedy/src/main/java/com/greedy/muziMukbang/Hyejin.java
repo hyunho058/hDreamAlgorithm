73c27442a17b2b91783e3f7d279054657ec87079
hyejinzz <hyejinj810@gmail.com>
2021-03-03 08:46:47 +0900

    firstinit

---
+++ code/01greedy/src/main/java/com/greedy/muziMukbang/Hyejin.java
@@ -0,0 +1,26 @@
+package com.greedy.muziMukbang;
+
+
+class Food implements Comparable<Food>{
+    private int time;
+    private int index;
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    //시간별 정렬
+    @Override
+    public int compareTo(Food o) {
+        return Integer.compare(this.time, o.time);
+    }
+}
+
+public class Hyejin {
+
+
+}
---
+++ code/02implementation/src/main/java/com/example/implementation/UpDownLeftRight/Hyejin.java
@@ -0,0 +1,4 @@
+package com.example.implementation.UpDownLeftRight;
+
+public class Hyejin {
+}
