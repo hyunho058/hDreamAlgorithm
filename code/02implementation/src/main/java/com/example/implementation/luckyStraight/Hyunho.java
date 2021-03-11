4fa3bf03542be199add8c054d5aafb1bc457ad72
hyunho058 <hyunho058@naver.com>
2021-03-11 09:58:32 +0900

    Add LuckyStraight

---
+++ code/02implementation/src/main/java/com/example/implementation/luckyStraight/Hyunho.java
@@ -0,0 +1,27 @@
+package com.example.implementation.luckyStraight;
+
+import java.util.Scanner;
+
+public class Hyunho {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String mNumList = scanner.next();
+        System.out.println("RESULT = "+skill(mNumList));
+    }
+
+    public static String skill(String numList){
+        int leftSum = 0;
+        int rightSum = 0;
+        //홀수 짝수 조건 확인
+        if (numList.length()%2 != 0){
+            return "Odd number";
+        }else {
+            //왼쪽, 오른쪽 숫자 더하기
+            for (int i = 0; i < numList.length()/2; i++){
+                leftSum += Integer.parseInt(numList.charAt(i)+"");
+                rightSum += Integer.parseInt(numList.charAt(i+(numList.length()/2))+"");
+            }
+            return leftSum == rightSum ? "LUCKY":"READY";
+        }
+    }
+}
