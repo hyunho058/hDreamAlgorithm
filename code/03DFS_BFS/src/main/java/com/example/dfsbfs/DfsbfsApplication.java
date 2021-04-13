26e7c63c7d270cf35b257be6fa004682428eb5ce
hyejinzz <hyejinj810@gmail.com>
2021-04-13 21:08:52 +0900

    dfs-bfs first commit

---
+++ code/03DFS_BFS/src/main/java/com/example/dfsbfs/DfsbfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dfsbfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsbfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfsbfsApplication.class, args);
+    }
+
+}
---
+++ code/03DFS_BFS/src/test/java/com/example/dfsbfs/DfsbfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dfsbfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsbfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
