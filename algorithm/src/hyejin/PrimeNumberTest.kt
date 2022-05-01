4cab9d4007dd8a6080e7b47241f2d512ab3871b9
hyejinjeong9999 <hyejinj810@gmail.com>
2022-05-01 21:58:09 +0900

    소수 만들기

---
+++ algorithm/src/hyejin/PrimeNumberTest.kt
@@ -0,0 +1,14 @@
+package hyejin
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class PrimeNumberTest {
+
+    @Test
+    fun solution() {
+
+       var primeNumber = PrimeNumber()
+       primeNumber.solution(intArrayOf(1,2,3,4))
+    }
+}
