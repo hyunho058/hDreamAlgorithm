51f7c746c58b3c248fe313aed84b11a06c2d3a0f
Summer0411 <summer@funnc.com>
2022-05-10 22:44:18 +0900

    gradle설정변경

--- src/main/java/com/example/hdreamalgorithm/greedy/coinzero/Hyejin.kt
+++ src/main/java/com/example/hdreamalgorithm/greedy/coinzero/Hyejin.kt
@@ -1,4 +1,5 @@
 package com.example.hdreamalgorithm.greedy.coinzero
 
 class Hyejin {
+
 }
---
+++ src/main/java/com/example/hdreamalgorithm/greedy/coinzero/Hyunho.java
@@ -0,0 +1,8 @@
+package com.example.hdreamalgorithm.greedy.coinzero;
+
+public class Hyunho {
+
+    public static void main(String[] args) {
+        System.out.printf(" ");
+    }
+}
