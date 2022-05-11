dae6246b0c7305f027e24b6cf98f9557a5a489ce
hyunho058 <hyunho058@naver.com>
2022-05-11 23:49:16 +0900

    백준 No2875(대회or인턴) 문제 풀이

--- src/main/java/com/example/hdreamalgorithm/greedy/competition/Hyunho.java
+++ src/main/java/com/example/hdreamalgorithm/greedy/competition/Hyunho.java
@@ -2,6 +2,9 @@ package com.example.hdreamalgorithm.greedy.competition;
 
 import java.util.Scanner;
 
+//test case//
+// 6 3 2 => 2
+// 2 1 1 => 0
 public class Hyunho {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,13 +14,22 @@ public class Hyunho {
         int teamCCount = 0;
         teamCCount = createTeam(n, m, teamCCount);
         System.out.println("teamCCount = " + teamCCount);
-        // 6 3 2 => 2
-        // 2 1 1 => 0
+
+        int remain = n - (teamCCount * 2) + (m - teamCCount);
+        System.out.println("remain = " + remain);
+        if (remain < k) {
+            k = k - remain;
+            while (k > 0){
+                teamCCount--;
+                k = k -3;
+            }
+        }
+        System.out.println("teamCCount = " + teamCCount);
     }
 
     public static int createTeam(int n, int m, int teamCount) {
         System.out.println("n = " + n + ", m = " + m + ", teamCount = " + teamCount);
-        if (n >= 2) {
+        if (n >= 2 && m > 0) {
             n = n - 2;
             m--;
             teamCount++;
